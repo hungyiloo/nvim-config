82c5e01590d52df167dc8685337e7fe2028ef21e
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-06-04 23:28:22 +0700

    Switch to gx.nvim instead of using BetterGX.nvim

--- lua/plugins/better-gx.lua
+++ lua/plugins/better-gx.lua
@@ -1,12 +1,10 @@
 return {
-  "TobinPalmer/BetterGX.nvim",
-  keys = {
-    {
-      "g<cr>",
-      function()
-        require("better-gx").BetterGx()
-      end,
-      desc = "Open file/link under cursor",
-    },
-  },
+  "chrishrb/gx.nvim",
+  keys = { { "g<cr>", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+  cmd = { "Browse" },
+  init = function()
+    vim.g.netrw_nogx = 1 -- disable netrw gx
+  end,
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = true, -- default settings
 }
