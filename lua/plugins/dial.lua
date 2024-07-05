0c6a73de428b0a11116475ad6d3fb3f63385afe6
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-07-05 21:56:18 +0800

    fix: english short day name increment/decrement support
    The previous implementation assumed that the day name was related to a
    "date", which is not always the case. Dial.nvim didn't like short day
    names that were disconnected in this way, so we define a new constant
    augent that is just cyclic between all the short day names.
    
    There may be a better way to do this, because the short day names are
    actually defined internally within dial.nvim code; I just don't know
    whether a cycling constant augend is defined for them and how to expose
    it in the right places. It's defined for the full day names, because
    it's working, but I can't find the code!

--- lua/plugins/dial.lua
+++ lua/plugins/dial.lua
@@ -5,20 +5,26 @@ return {
     opts = function(_, opts)
       local augend = require("dial.augend")
       -- Add "yyyy-mm-dd" dial support to markdown files
-      table.insert(opts.groups.markdown, augend.date.alias["%Y-%m-%d"])
+      table.insert(opts.groups.default, augend.date.alias["%Y-%m-%d"])
 
-      -- Add short day name dial support to markdown files
-      table.insert(opts.groups.markdown, augend.date.new{
-        pattern = "%a",
-        default_kind = "day",
-        -- if true, it does not match dates which does not exist, such as 2022/05/32
-        only_valid = true,
-        -- if true, it only matches dates with word boundary
+      -- Add english *short* day name dial support to markdown files.
+      -- This is necessary for short day names not connected to an actual date.
+      table.insert(opts.groups.default, augend.constant.new{
+        elements = {
+          "Mon",
+          "Tue",
+          "Wed",
+          "Thu",
+          "Fri",
+          "Sat",
+          "Sun",
+        },
         word = true,
+        cyclic = true,
       })
 
       -- Add "yyyy-mm-dd dayname" dial support to markdown files
-      table.insert(opts.groups.markdown, augend.date.new{
+      table.insert(opts.groups.default, augend.date.new{
         pattern = "%Y-%m-%d %a",
         default_kind = "day",
         -- if true, it does not match dates which does not exist, such as 2022/05/32
