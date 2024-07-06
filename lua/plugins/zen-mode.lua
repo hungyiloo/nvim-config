48e0b0c8558b8c8249f53b7b047dfb6315d084d2
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-07-06 22:13:36 +0800

    feat: customize the new Neovide params in zen-mode plugin

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -1,6 +1,21 @@
 return {
   "folke/zen-mode.nvim",
-  opts = {},
+  opts = {
+    plugins = {
+      neovide = {
+        enabled = true,
+        scale = 1.25,
+        disable_animations = {
+          neovide_animation_length = 0.13,
+          neovide_cursor_animate_command_line = false,
+          neovide_scroll_animation_length = 0.13,
+          neovide_position_animation_length = 0.13,
+          neovide_cursor_animation_length = 0.13,
+          neovide_cursor_vfx_mode = "railgun",
+        }
+      }
+    }
+  },
   keys = {
     {
       "<leader>uz",
