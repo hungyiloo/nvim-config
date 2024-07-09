cf4c7efe638f59d7d31b417db62dec50fb853307
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-07-09 18:59:32 +0800

    fix!: rework flash keymaps to be less confusing whent using "s" for mini.surround

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,10 +2,20 @@ return {
   "folke/flash.nvim",
   -- stylua: ignore
   keys = {
-    { "S", mode = {"n", "o", "x" }, false },
-    { "g/", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    -- Disable LazyVim's default "s" related keymaps for flash because they
+    -- conflict with my muscle memory for mini.surround (which I use a lot more)
+    { "s", mode = { "n", "x", "o" }, false },
+    { "S", mode = { "n", "o", "x" }, false },
+
+    -- Instead, replace them with backslash for easy access.
+    -- I'm hoping that putting them on an easy to reach keymap will encourage
+    -- me to use it more, as I currently don't
+    { "\\", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "g\\", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+
+    -- These are the same as the defaults
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    { "<c-s>", mode = "c", function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
 }
