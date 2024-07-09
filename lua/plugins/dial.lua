f7e967c680ebfb8ff25782fc3fb1157f3062dcbb
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-07-09 18:58:28 +0800

    fix: custom date dials must be defined in both global and markdown groups

--- lua/plugins/dial.lua
+++ lua/plugins/dial.lua
@@ -4,12 +4,15 @@ return {
     "monaqa/dial.nvim",
     opts = function(_, opts)
       local augend = require("dial.augend")
-      -- Add "yyyy-mm-dd" dial support to markdown files
-      table.insert(opts.groups.default, augend.date.alias["%Y-%m-%d"])
 
-      -- Add english *short* day name dial support to markdown files.
+      -- Add "yyyy-mm-dd" dial support
+      local isoDateStamps = augend.date.alias["%Y-%m-%d"]
+      table.insert(opts.groups.default, isoDateStamps)
+      table.insert(opts.groups.markdown, isoDateStamps)
+
+      -- Add english *short* day name dial support
       -- This is necessary for short day names not connected to an actual date.
-      table.insert(opts.groups.default, augend.constant.new{
+      local shortDayNames = augend.constant.new{
         elements = {
           "Mon",
           "Tue",
@@ -21,17 +24,21 @@ return {
         },
         word = true,
         cyclic = true,
-      })
+      }
+      table.insert(opts.groups.default, shortDayNames)
+      table.insert(opts.groups.markdown, shortDayNames)
 
-      -- Add "yyyy-mm-dd dayname" dial support to markdown files
-      table.insert(opts.groups.default, augend.date.new{
+      -- Add "yyyy-mm-dd dayname" dial support
+      local dateWithShortDay = augend.date.new{
         pattern = "%Y-%m-%d %a",
         default_kind = "day",
         -- if true, it does not match dates which does not exist, such as 2022/05/32
         only_valid = true,
         -- if true, it only matches dates with word boundary
         word = true,
-      })
+      }
+      table.insert(opts.groups.default, dateWithShortDay)
+      table.insert(opts.groups.markdown, dateWithShortDay)
     end,
   },
 }
