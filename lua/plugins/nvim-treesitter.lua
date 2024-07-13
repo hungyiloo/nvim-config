621107b358968d3cc265456891968cc29f61f425
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-07-13 11:58:28 +0800

    fix: jsx/tsx treesitter issues by pinning to old version of nvim-treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,16 +1,24 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  --
+  -- NOTE: Pinned to this commit because of these issues:
+  -- https://github.com/tree-sitter/tree-sitter-javascript/issues/329
+  -- also briefly mentioned here: https://github.com/nvim-treesitter/nvim-treesitter/pull/6873
+  -- but the nvim-treesitter maintaners forgot to hold off on updating
+  -- typescript/tsx, which is related to the javascript parser.
+  commit = "53c79ddc28b0df3263d629f5e20c9283b05841ca",
+
   config = function(_, opts)
     if type(opts.ensure_installed) == "table" then
       opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
     end
     require("nvim-treesitter.configs").setup(opts)
 
-    -- NOTE: This adds numbat support.
-    --       Trying to figure out why this isn't working?
-    --       1. Run :TSInstall numbat
-    --       2. Download the highlights.scm file from the repo linked below
-    --          into /nvim-data/lazy/nvim-treesitter/queries/
+    -- This adds numbat support.
+    -- Trying to figure out why this isn't working?
+    -- 1. Run :TSInstall numbat
+    -- 2. Download the highlights.scm file from the repo linked below
+    --    into /nvim-data/lazy/nvim-treesitter/queries/
     local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
     ---@diagnostic disable-next-line: inject-field
     parser_config.numbat = {
@@ -29,17 +37,5 @@ return {
       },
     })
     vim.treesitter.language.register("numbat", "numbat")
-
-
-    -- Ensure angular HTML files are set to an angular-specific filetype
-    -- Note that this depends on the angular LazyVim extra being enabled.
-    -- TODO: Refactor this autocmd and the angular extra setup into one file
-    --       for clarity
-    vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
-      pattern = { "*.component.html", "*.container.html" },
-      callback = function()
-        vim.bo.filetype = "angular"
-      end,
-    })
   end,
 }
