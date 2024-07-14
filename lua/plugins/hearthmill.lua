6e70abc4ecc9221b0505ab4c512d879b26b91974
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-07-14 12:47:51 +0800

    feat: new hearthmill keymappings and groups

--- lua/plugins/hearthmill.lua
+++ lua/plugins/hearthmill.lua
@@ -1,34 +1,52 @@
 return {
   "hearthmill",
   dev = true,
-  config = true,
+  config = function (_, opts)
+    require("hearthmill").setup(opts)
+
+    -- Add a new +hearthmill keymap group
+    local wk = require("which-key")
+    wk.add({
+      { "gh",  name = "hearthmill" },
+      { "ghe",  name = "element" },
+      { "gha",  name = "attribute" },
+      { "ght",  name = "tag" },
+    })
+  end,
   -- stylua: ignore
   keys = {
-    { mode = "n", "ghes", function() require("hearthmill").select("element") end, desc = "Element Select" },
-    { mode = "n", "gheb", function() require("hearthmill").goto_beginning("element") end, desc = "Element Beginning" },
-    { mode = "n", "ghee", function() require("hearthmill").goto_end("element") end, desc = "Element End" },
-    { mode = "n", "ghen", function() require("hearthmill").goto_next("element") end, desc = "Next Element" },
-    { mode = "n", "ghep", function() require("hearthmill").goto_prev("element") end, desc = "Previous Element" },
-    { mode = "n", "ghed", function() require("hearthmill").delete("element") end, desc = "Element Delete" },
-    { mode = "n", "ghet", function() require("hearthmill").transpose("element") end, desc = "Element Transpose" },
-    { mode = "n", "ghev", function() require("hearthmill").vanish() end, desc = "Element Vanish" },
-    { mode = "n", "gher", function() require("hearthmill").rename() end, desc = "Element Rename" },
-    { mode = { "n", "o", "x" }, "ghew", function() require("hearthmill").wrap() end, desc = "Element Wrap" },
+    { mode = { "n", "x" }, "ghes", function() require("hearthmill").select("element") end, desc = "[e]lement [s]elect" },
+    { mode = { "n", "x" }, "ghec", function() require("hearthmill").select_contents("element") end, desc = "[e]lement select [c]ontents" },
+    { mode = { "n", "x" }, "gheb", function() require("hearthmill").goto_beginning("element") end, desc = "[e]lement [b]eginning" },
+    { mode = { "n", "x" }, "ghee", function() require("hearthmill").goto_end("element") end, desc = "[e]lement [e]nd" },
+    { mode = { "n", "x" }, "ghen", function() require("hearthmill").goto_next("element") end, desc = "[e]lement [n]ext" },
+    { mode = { "n", "x" }, "ghep", function() require("hearthmill").goto_prev("element") end, desc = "[e]lement [p]revious" },
+    { mode = { "n", "x" }, "gheo", function() require("hearthmill").goto_parent_element() end, desc = "[e]lement [o]uter parent" },
+    { mode = "n", "ghed", function() require("hearthmill").delete("element") end, desc = "[e]lement [d]elete" },
+    { mode = "n", "ghet", function() require("hearthmill").transpose("element") end, desc = "[e]lement [t]ranspose" },
+    { mode = "n", "gheT", function() require("hearthmill").transpose_backward("element") end, desc = "[e]lement [T]ranspose backwards" },
+    { mode = "n", "gher", function() require("hearthmill").rename() end, desc = "[e]lement [r]ename" },
+    { mode = { "n", "x" }, "ghew", function() require("hearthmill").wrap() end, desc = "[e]lement [w]rap" },
+    { mode = "n", "gheu", function() require("hearthmill").unwrap() end, desc = "[e]lement [u]nwrap" },
+    { mode = "n", "ghe=", function() require("hearthmill").clone("element") end, desc = "[e]lement clone [=]" },
 
-    { mode = "n", "ghas", function() require("hearthmill").select("attribute") end, desc = "Attribute Select" },
-    { mode = "n", "ghab", function() require("hearthmill").goto_beginning("attribute") end, desc = "Attribute Beginning" },
-    { mode = "n", "ghae", function() require("hearthmill").goto_end("attribute") end, desc = "Attribute End" },
-    { mode = "n", "ghan", function() require("hearthmill").goto_next("attribute") end, desc = "Next Attribute" },
-    { mode = "n", "ghap", function() require("hearthmill").goto_prev("attribute") end, desc = "Previous Attribute" },
-    { mode = "n", "ghad", function() require("hearthmill").delete("attribute") end, desc = "Attribute Delete" },
-    { mode = "n", "ghat", function() require("hearthmill").transpose("attribute") end, desc = "Attribute Transpose" },
+    { mode = { "n", "x" }, "ghas", function() require("hearthmill").select("attribute") end, desc = "[a]ttribute [s]elect" },
+    { mode = { "n", "x" }, "ghac", function() require("hearthmill").select_contents("attribute") end, desc = "[a]ttribute select [c]ontents" },
+    { mode = { "n", "x" }, "ghab", function() require("hearthmill").goto_beginning("attribute") end, desc = "[a]ttribute [b]eginning" },
+    { mode = { "n", "x" }, "ghae", function() require("hearthmill").goto_end("attribute") end, desc = "[a]ttribute [e]nd" },
+    { mode = { "n", "x" }, "ghan", function() require("hearthmill").goto_next("attribute") end, desc = "[a]ttribute [n]ext" },
+    { mode = { "n", "x" }, "ghap", function() require("hearthmill").goto_prev("attribute") end, desc = "[a]ttribute [p]revious" },
+    { mode = "n", "ghad", function() require("hearthmill").delete("attribute") end, desc = "[a]ttribute [d]elete" },
+    { mode = "n", "ghat", function() require("hearthmill").transpose("attribute") end, desc = "[a]ttribute [t]ranspose" },
+    { mode = "n", "ghaT", function() require("hearthmill").transpose_backward("attribute") end, desc = "[a]ttribute [T]ranspose" },
+    { mode = "n", "gha=", function() require("hearthmill").clone("attribute") end, desc = "[a]ttribute clone [=]" },
 
-    { mode = "n", "ghts", function() require("hearthmill").select("tag") end, desc = "Tag Select" },
-    { mode = "n", "ghtb", function() require("hearthmill").goto_beginning("tag") end, desc = "Tag Beginning" },
-    { mode = "n", "ghte", function() require("hearthmill").goto_end("tag") end, desc = "Tag End" },
-    { mode = "n", "ghtn", function() require("hearthmill").goto_next("tag") end, desc = "Next Tag" },
-    { mode = "n", "ghtp", function() require("hearthmill").goto_prev("tag") end, desc = "Previous Tag" },
-    { mode = "n", "ghtd", function() require("hearthmill").delete("tag") end, desc = "Tag Delete" },
-    { mode = "n", "ghtv", function() require("hearthmill").vanish() end, desc = "Tag (Element) Vanish" },
+    { mode = { "n", "x" }, "ghts", function() require("hearthmill").select("tag") end, desc = "[t]ag [s]elect" },
+    { mode = { "n", "x" }, "ghtc", function() require("hearthmill").select_contents("tag") end, desc = "[t]ag select [c]ontents" },
+    { mode = { "n", "x" }, "ghtb", function() require("hearthmill").goto_beginning("tag") end, desc = "[t]ag [b]eginning" },
+    { mode = { "n", "x" }, "ghte", function() require("hearthmill").goto_end("tag") end, desc = "[t]ag [e]nd" },
+    { mode = { "n", "x" }, "ghtn", function() require("hearthmill").goto_next("tag") end, desc = "[t]ag [n]ext" },
+    { mode = { "n", "x" }, "ghtp", function() require("hearthmill").goto_prev("tag") end, desc = "[t]ag [p]revious" },
+    { mode = "n", "ghtd", function() require("hearthmill").delete("tag") end, desc = "[t]ag [d]elete" },
   }
 }
