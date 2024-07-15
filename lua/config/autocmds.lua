a5af5baaf0f7a4c6a29f806dbc765caca35d62ce
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-07-15 22:49:20 +0800

    fix: angular template files need a more stable filetype to work well

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,13 +2,19 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-
 -- Ensure angular HTML files are set to an angular-specific filetype
 -- Note that this depends on the angular LazyVim extra being enabled.
 -- See ../plugins/angular.lua for other related setup
 vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
   pattern = { "*.component.html", "*.container.html" },
   callback = function()
-    vim.bo.filetype = "angular"
+    -- BUG: This seems to break angularls from attaching to these files
+    -- vim.bo.filetype = "angular"
+
+    -- so instead we set it to something more stable for now.
+    -- NOTE: Could be a neovim nightly issue, actually!
+    -- I've noticed the filetype seems to be set by default to something weird:
+    -- "htmlangular" which breaks a lot more than just LSP.
+    vim.bo.filetype = "html"
   end,
 })
