3924269337322d2697d5d86bdf67ad828cf6fce7
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-07-17 18:15:46 +0800

    docs: refer to an issue with the botched htmlangular filetype upstream

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -9,12 +9,14 @@ vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
   pattern = { "*.component.html", "*.container.html" },
   callback = function()
     -- BUG: This seems to break angularls from attaching to these files
+    --      even though it provides better syntax highlighting.
     -- vim.bo.filetype = "angular"
 
     -- so instead we set it to something more stable for now.
     -- NOTE: Could be a neovim nightly issue, actually!
     -- I've noticed the filetype seems to be set by default to something weird:
     -- "htmlangular" which breaks a lot more than just LSP.
+    -- https://github.com/vim/vim/pull/13594#issuecomment-1834465890
     vim.bo.filetype = "html"
   end,
 })
