0bf7febb26241904c69de6870011221cea5b21aa
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-07-22 17:04:33 +0800

    feat: switch back to upstream of gx.nvim, as my PR was merged

--- lua/plugins/gx.lua
+++ lua/plugins/gx.lua
@@ -1,10 +1,5 @@
 return {
-  "hungyiloo/gx.nvim",
-  --
-  -- NOTE: This is a commit on my fork which fixes some Windows issues.
-  --       Remove this if my PR is merged upstream.
-  commit = "8854d7f28d513050ea736a6d6d1f7605c82e9813",
-
+  "chrishrb/gx.nvim",
   lazy = true,
   keys = { { "g<cr>", "<cmd>Browse<cr>", mode = { "n", "x" } } },
   cmd = { "Browse" },
