2ff7b497725f5905bfb030dcaecf701a05920b20
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-07-24 20:46:45 +0800

    fix: rely more on upstream uptake of htmlangular support

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -31,12 +31,19 @@ return {
     })
     vim.treesitter.language.register("numbat", "numbat")
 
-    -- NOTE: Pinned TSX to this commit because of these issues:
-    -- https://github.com/tree-sitter/tree-sitter-javascript/issues/329
-    -- also briefly mentioned here: https://github.com/nvim-treesitter/nvim-treesitter/pull/6873
-    -- but the nvim-treesitter maintaners forgot to hold off on updating
-    -- typescript/tsx, which is depends on the broken updated javascript parser.
+    -- HACK: Pinned TSX to this commit because of these issues:
+    --       https://github.com/tree-sitter/tree-sitter-javascript/issues/329
+    --       also briefly mentioned here: https://github.com/nvim-treesitter/nvim-treesitter/pull/6873
+    --       but the nvim-treesitter maintaners forgot to hold off on updating
+    --       typescript/tsx, which is depends on the broken updated javascript parser.
     parser_config.tsx.install_info.revision = "4f3eb6655a1cd1a1f87ef10201f8e22886dcd76e"
+
+    -- NOTE: Fix angular treesitter parsing for style bindings including a percent CSS unit extension.
+    --       (This is my own fork of the angular treesitter parser)
+    --       This coincidentally fixes inconsistent behavior in nvim-ts-autotag in htmlangular templates
+    parser_config.angular.install_info.url = "https://github.com/hungyiloo/tree-sitter-angular"
+    parser_config.angular.install_info.branch = "fix-style-binding-percent-unit"
+    parser_config.angular.install_info.revision = "6cbab4576f6b17b70aca990b1d6fe55bfcdb4669"
 
     require("nvim-treesitter.configs").setup(opts)
   end,
