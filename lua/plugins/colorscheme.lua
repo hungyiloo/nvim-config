62810c3d29201b405b9936d3fc72ad6d367ac1a9
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-07-27 15:50:52 +0800

    refactor: move custom markdown highlights into catppuccin config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,6 +13,16 @@ return {
           LspReferenceText = { bg = colors.surface0 }, -- originally surface1
           LspReferenceRead = { bg = colors.surface0 },
           LspReferenceWrite = { bg = colors.surface0 },
+
+          -- custom highlights for markdown backgrounds
+          RenderMarkdownH1Bg = { bg = "#301b21" },
+          RenderMarkdownH2Bg = { bg = "#32231b" },
+          RenderMarkdownH3Bg = { bg = "#312d23" },
+          RenderMarkdownH4Bg = { bg = "#212d20" },
+          RenderMarkdownH5Bg = { bg = "#17272f" },
+          RenderMarkdownH6Bg = { bg = "#1f2447" },
+          RenderMarkdownCode = { bg = "#181926" },
+          RenderMarkdownCodeInline = { bg = "#181926" },
         }
       end
       if not vim.g.neovide then
--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -15,18 +15,5 @@ return {
     opts = function(_, opts)
       opts.code.border = "thick" -- thin borders don't look great with my environment/font
     end,
-    config = function(_, opts)
-      -- Markdown background highlight colors based on catppuccin color scheme
-      vim.cmd([[highlight RenderMarkdownH1Bg guibg=#301b21]])
-      vim.cmd([[highlight RenderMarkdownH2Bg guibg=#32231b]])
-      vim.cmd([[highlight RenderMarkdownH3Bg guibg=#312d23]])
-      vim.cmd([[highlight RenderMarkdownH4Bg guibg=#212d20]])
-      vim.cmd([[highlight RenderMarkdownH5Bg guibg=#17272f]])
-      vim.cmd([[highlight RenderMarkdownH6Bg guibg=#36394c]])
-      vim.cmd([[highlight RenderMarkdownCode guibg=#181926]])
-      vim.cmd([[highlight RenderMarkdownCodeInline guibg=#181926]])
-
-      require("render-markdown").setup(opts)
-    end,
   },
 }
