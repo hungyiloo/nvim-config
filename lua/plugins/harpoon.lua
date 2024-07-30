d9ba28548e4ee8889bec17a3b53a476130aeabc6
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-07-30 18:52:02 +0800

    feat!: disable harpoon for now as it doesn't work well for me

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,9 +1,10 @@
 return {
-  { import = "lazyvim.plugins.extras.editor.harpoon2" },
-  {
-    "ThePrimeagen/harpoon",
-    -- HACK: Pinning to this commit prevents harpoons from being deleted after
-    --       neovim closure. See https://github.com/ThePrimeagen/harpoon/issues/577
-    commit = "e76cb03",
-  },
+  -- Harpoon just doesn't work well for me
+  -- { import = "lazyvim.plugins.extras.editor.harpoon2" },
+  -- {
+  --   "ThePrimeagen/harpoon",
+  --   -- Pinning to this commit prevents harpoons from being deleted after
+  --   -- neovim closure. See https://github.com/ThePrimeagen/harpoon/issues/577
+  --   commit = "e76cb03",
+  -- },
 }
