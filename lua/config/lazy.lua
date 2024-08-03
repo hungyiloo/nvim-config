c8930044f3d90d64d805f6df658ea7356c995e8f
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-08-03 17:25:22 +0800

    style: remove unnecessary commented out code

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,7 +16,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
-    -- { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "plugins" },
