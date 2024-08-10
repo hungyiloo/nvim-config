2fa87e6833b74784ef365641f7db1e5d83f5693f
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-08-10 21:44:20 +0800

    fix: more descriptive oil keymap desc

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,6 +7,6 @@ return {
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
-    { "<leader>f.", mode = "n", "<cmd>Oil --float<cr>", desc = "Browse with oil", silent = true, noremap = true },
+    { "<leader>f.", mode = "n", "<cmd>Oil --float<cr>", desc = "Directory Editor", silent = true, noremap = true },
   },
 }
