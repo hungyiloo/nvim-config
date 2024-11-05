f0d2e78455d5c61a5a3c43aeb19557beabde1f72
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-11-05 16:26:42 +0700

    chore: remove noice pin (upstream bugs fixed)

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,6 +1,3 @@
 return {
   "folke/noice.nvim",
-  -- HACK: Pinning to this old commit because anything newer than this causes
-  --       cursor flickering issues when entering commands
-  commit = "d9328ef903168b6f52385a751eb384ae7e906c6f"
 }
