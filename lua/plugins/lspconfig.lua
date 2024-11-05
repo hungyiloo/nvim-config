c28741d3429c9b65b6e3e80298ca32e169a97d06
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-11-05 16:27:45 +0700

    feat: separate deno/vtsls project detection

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -5,6 +5,15 @@ return {
       html = {
         filetypes = { "html", "templ", "htmlangular" },
       },
+      denols = {
+        filetypes = { "typescript", "typescriptreact" },
+        root_dir = function(...)
+          return require("lspconfig").util.root_pattern("deno.jsonc", "deno.json")(...)
+        end,
+      },
+      vtsls = {
+        root_dir = require("lspconfig").util.root_pattern("package.json"),
+      },
     },
   },
 }
