cae01f99468fd047363db60e2856e5fcfdd32bdb
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-11-06 09:38:36 +0700

    feat: better keybindings and styles for multicursor

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -2,29 +2,32 @@ return {
   "jake-stewart/multicursor.nvim",
   branch = "1.0",
   keys = {
-    { mode = { "n", "v" }, "gm<space>", function ()
+    { mode = { "n" }, "<esc>", function ()
       local mc = require("multicursor-nvim")
       if not mc.cursorsEnabled() then
         mc.enableCursors()
       elseif mc.hasCursors() then
         mc.clearCursors()
+      else
+        vim.cmd("noh")
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<esc>", true, true, true), "n", true)
       end
     end },
     { mode = { "n" },      "gm<backspace>", function () require("multicursor-nvim").disableCursors() end     },
     { mode = { "n" },      "gmk",           function () require("multicursor-nvim").lineAddCursor(-1) end    },
-    { mode = { "n" },      "<up>",          function () require("multicursor-nvim").lineAddCursor(-1) end    },
-    { mode = { "n" },      "<S-up>",        function () require("multicursor-nvim").lineSkipCursor(-1) end   },
+    { mode = { "n" },      "<S-up>",        function () require("multicursor-nvim").lineAddCursor(-1) end    },
+    { mode = { "n" },      "<C-S-up>",      function () require("multicursor-nvim").lineSkipCursor(-1) end   },
     { mode = { "n" },      "gmj",           function () require("multicursor-nvim").lineAddCursor(1) end     },
-    { mode = { "n" },      "<down>",        function () require("multicursor-nvim").lineAddCursor(1) end     },
-    { mode = { "n" },      "<S-down>",      function () require("multicursor-nvim").lineSkipCursor(1) end    },
+    { mode = { "n" },      "<S-down>",      function () require("multicursor-nvim").lineAddCursor(1) end     },
+    { mode = { "n" },      "<C-S-down>",    function () require("multicursor-nvim").lineSkipCursor(1) end    },
     { mode = { "n", "v" }, "gmm",           function () require("multicursor-nvim").matchAddCursor(1) end    },
-    { mode = { "n", "v" }, "<right>",       function () require("multicursor-nvim").matchAddCursor(1) end    },
+    { mode = { "n", "v" }, "<S-right>",     function () require("multicursor-nvim").matchAddCursor(1) end    },
     { mode = { "n", "v" }, "gmn",           function () require("multicursor-nvim").matchSkipCursor(1) end   },
-    { mode = { "n", "v" }, "<S-right>",     function () require("multicursor-nvim").matchSkipCursor(1) end   },
+    { mode = { "n", "v" }, "<C-S-right>",   function () require("multicursor-nvim").matchSkipCursor(1) end   },
     { mode = { "n", "v" }, "gmM",           function () require("multicursor-nvim").matchAddCursor(-1) end   },
-    { mode = { "n", "v" }, "<left>",        function () require("multicursor-nvim").matchAddCursor(-1) end   },
+    { mode = { "n", "v" }, "<S-left>",      function () require("multicursor-nvim").matchAddCursor(-1) end   },
     { mode = { "n", "v" }, "gmN",           function () require("multicursor-nvim").matchSkipCursor(-1) end  },
-    { mode = { "n", "v" }, "<S-left>",      function () require("multicursor-nvim").matchSkipCursor(-1) end  },
+    { mode = { "n", "v" }, "<C-S-left>",    function () require("multicursor-nvim").matchSkipCursor(-1) end  },
     { mode = { "n" },      "gmg",           function () require("multicursor-nvim").matchAllAddCursors() end },
     { mode = { "n" },      "gm]",           function () require("multicursor-nvim").nextCursor() end         },
     { mode = { "n" },      "gm[",           function () require("multicursor-nvim").prevCursor() end         },
@@ -47,11 +50,15 @@ return {
 
     -- Customize how cursors look.
     local hl = vim.api.nvim_set_hl
-    hl(0, "MultiCursorCursor", { link = "Cursor" })
     hl(0, "MultiCursorVisual", { link = "Visual" })
     hl(0, "MultiCursorSign", { link = "SignColumn" })
     hl(0, "MultiCursorDisabledCursor", { link = "Visual" })
     hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
     hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
+    --
+    -- Use the following line to make the multicursor cursor visually identical to the real cursor
+    -- hl(0, "MultiCursorCursor", { link = "Cursor" })
+    -- I prefer a customised look to differentiate them from the real cursor
+    hl(0, "MultiCursorCursor", { foreground = require("catppuccin/palettes/mocha").crust, background = require("catppuccin/palettes/mocha").mauve })
   end,
 }
