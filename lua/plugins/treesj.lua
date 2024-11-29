05443c6c9158a0938e8afa2dd4bf83ac87ca499e
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-11-29 22:24:29 +0700

    feat: add treesj for treesitter-powered split/join

---
+++ lua/plugins/treesj.lua
@@ -0,0 +1,11 @@
+return {
+  "Wansmer/treesj",
+  keys = {
+    { "g|", "<cmd>TSJToggle<cr>", mode = { "n" }, desc = "Treesitter Split/Join" },
+  },
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  opts = {
+    max_join_length = 1024,
+  },
+  config = true,
+}
