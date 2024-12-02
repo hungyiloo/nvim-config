8672f6b2fa8cafcb48ea53b772fb8ca83bf82297
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-12-02 12:02:26 +0700

    fix: stop new snacks keymaps from interfering with my personal keymaps

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/snacks.nvim",
+  keys = {
+    { "<leader>.",  false },
+    { "<leader>S",  false},
+    { "<leader>bs",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+    { "<leader>bS",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+  }
+}
