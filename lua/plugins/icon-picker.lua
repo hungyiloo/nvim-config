c6f779c484c208e3b61a34f09af7c50ab482dda0
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-12-15 21:51:10 +0700

    fix!: replace icon-picker with trio of more stable plugins

--- lua/plugins/icon-picker.lua
+++ lua/plugins/icon-picker.lua
@@ -1,35 +1,67 @@
 return {
-  "ziontee113/icon-picker.nvim",
-  keys = {
-    { mode = { "n" }, "<leader>ii", "<cmd>IconPickerNormal<cr>", desc = "Insert Icon", noremap = true, silent = true },
-    {
-      mode = { "n" },
-      "<leader>iu",
-      "<cmd>IconPickerNormal symbols<cr>",
-      desc = "Insert Symbol",
-      noremap = true,
-      silent = true,
-    },
-    {
-      mode = { "n" },
-      "<leader>ie",
-      "<cmd>IconPickerNormal emoji<cr>",
-      desc = "Insert Emoji",
-      noremap = true,
-      silent = true,
-    },
-    { mode = { "n" }, "<leader>iI", "<cmd>IconPickerYank<cr>", desc = "Yank Icon", noremap = true, silent = true }, --> Yank the selected icon into register
-    {
-      mode = { "i" },
-      "<C-x><C-x>i",
-      "<cmd>IconPickerInsert<cr>",
-      desc = "Insert Icon",
-      noremap = true,
-      silent = true,
-    },
+  {
+    "chrisbra/unicode.vim",
+    keys = {
+      {
+        mode = { "n" },
+        "<leader>iu",
+        function ()
+          vim.ui.input(
+            { prompt = "Filter unicode characters" },
+            function (query)
+              if not query then return end
+              vim.ui.select(
+                vim.fn['unicode#FindUnicodeBy'](query),
+                {
+                  prompt = "Select a unicode character",
+                  format_item = function (item)
+                    return item.name .. ": " .. item.glyph
+                  end
+                },
+                function (choice)
+                  if choice then
+                    vim.cmd("normal i" .. choice.glyph)
+                  end
+                end
+              )
+            end
+          )
+        end,
+        desc = "Insert Unicode",
+        noremap = true,
+        silent = true,
+      },
+    }
   },
-  opts = {
-    disable_legacy_commands = true,
+  {
+    "2kabhishek/nerdy.nvim",
+    keys = {
+      {
+        mode = { "n" },
+        "<leader>ii",
+        "<cmd>Nerdy<cr>",
+        desc = "Insert Icon",
+        noremap = true,
+        silent = true,
+      },
+    }
   },
-  config = true,
+  {
+    "allaman/emoji.nvim",
+    cmd = "EmojiInsert",
+    keys = {
+      {
+        mode = { "n" },
+        "<leader>ie",
+        "<cmd>EmojiInsert<cr>",
+        desc = "Insert Emoji",
+        noremap = true,
+        silent = true,
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = true
+  }
 }
