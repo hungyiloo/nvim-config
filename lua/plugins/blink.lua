20d43c86e16df85c82214d49a11cde7b5f5535d2
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-12-18 21:42:31 +0700

    feat: try out super-tab completion using blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,6 +1,11 @@
+---@diagnostic disable: missing-fields
 return {
   "saghen/blink.cmp",
+  --- @type blink.cmp.ConfigStrict
   opts = {
+    keymap = {
+      preset = "super-tab",
+    },
     completion = {
       trigger = {
         -- By default this is true, but this makes completion come up more
@@ -8,15 +13,15 @@ return {
         -- show_on_trigger_character = false
 
         -- Prevent commas from triggering completion.
-        -- I rarely need this behavior, and I can manually C-Space
-        show_on_blocked_trigger_characters = { ' ', '\n', '\t', ',' },
-        show_on_x_blocked_trigger_characters = { "'", '"', '(', ',' }
+        -- I rarely need this behavior, and I can manually C-Space.
+        -- show_on_insert_on_trigger_character = false,
+        -- show_on_blocked_trigger_characters = { ' ', '\n', '\t', ',' },
       },
       list = {
         -- While this isn't as convenient as "preselect", this reduces the
         -- amount of unwanted accidental completions when pressing the enter
         -- key while the completion menu happens to be showing.
-        -- selection = "manual"
+        selection = "preselect"
       }
     }
   }
