a60cb47e82119830057690e9b435744a5dd4cf9d
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-12-28 23:49:45 +0800

    fix(lsp): undo regression in lsp configs, and also ensure lspconfig lazy loading

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,26 +1,9 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      html = {
-        filetypes = { "html", "templ", "htmlangular" },
-      },
-      denols = {
-        filetypes = { "typescript", "typescriptreact" },
-        root_dir = function(...)
-          return require("lspconfig").util.root_pattern("deno.jsonc", "deno.json")(...)
-        end,
-      },
-      vtsls = {
-        root_dir = require("lspconfig").util.root_pattern("package.json"),
-      },
-    },
-  },
-  config = function (opts)
+  opts = function (_, opts)
     -- AutoHotkey LSP setup, which somehow enhances syntax highlighting too?
     require("lspconfig.configs").ahk2 = {
       default_config = {
-        autostart = true,
         cmd = {
           "node",
           vim.fn.expand("D:/dev/vscode-autohotkey2-lsp/server/dist/server.js"),
@@ -35,7 +18,26 @@ return {
         flags = { debounce_text_changes = 500 },
       }
     }
-    local nvim_lsp = require("lspconfig")
-    nvim_lsp.ahk2.setup({})
-  end
+    return vim.tbl_deep_extend(
+      "force",
+      opts,
+      {
+        servers = {
+          html = {
+            filetypes = { "html", "templ", "htmlangular" },
+          },
+          denols = {
+            filetypes = { "typescript", "typescriptreact" },
+            root_dir = function(...)
+              return require("lspconfig").util.root_pattern("deno.jsonc", "deno.json")(...)
+            end,
+          },
+          vtsls = {
+            root_dir = require("lspconfig").util.root_pattern("package.json"),
+          },
+          ahk2 = {}
+        },
+      }
+    )
+  end,
 }
