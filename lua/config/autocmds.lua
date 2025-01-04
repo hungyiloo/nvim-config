7f91a61e87e1f47b57a54bd1187d5ebfd6ca61c2
Hung-Yi Loo <hungyi.loo@gmail.com>
2025-01-04 15:58:13 +0800

    feat(neovide): disable cursor animations in terminal/fzf windows to prevent flicker

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,24 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function enable_cursor_animations(enable)
+  if enable then
+    vim.g.neovide_cursor_animation_length = 0.05
+    vim.g.neovide_cursor_vfx_mode = "railgun"
+  else
+    vim.g.neovide_cursor_animation_length = 0
+    vim.g.neovide_cursor_vfx_mode  = ""
+  end
+end
+
+-- disable cursor animations in terminal buffers, as they flicker a lot
+-- disable cursor animations in fzf windows, as they flicker a lot
+vim.api.nvim_create_autocmd({"BufEnter", "WinNew"}, {
+  callback = function()
+    vim.schedule(function ()
+      enable_cursor_animations(vim.bo.buftype ~= "terminal" and vim.bo.filetype ~= "fzf")
+    end)
+  end,
+  group = vim.api.nvim_create_augroup("MyCursorAnimations", { clear = true }),
+})
