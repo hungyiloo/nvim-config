09edc1f40228eea19d4ed96072de15ca7b62ee91
Hung-Yi Loo <hungyi.loo@gmail.com>
2025-01-04 15:58:53 +0800

    fix(oil): no critical need to have Oil load on startup

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,7 +1,7 @@
 return {
   "stevearc/oil.nvim",
   -- Can't be lazy if we want it to replace netrw by default at startup
-  lazy = false,
+  -- lazy = false,
   cmd = "Oil",
   opts = {},
   -- Optional dependencies
