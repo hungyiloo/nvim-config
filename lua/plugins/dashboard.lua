fb5ea299a90dba09c045131dc1c303284d33d42d
Hung-Yi Loo <hungyi.loo@gmail.com>
2025-01-25 19:16:14 +0800

    feat(dashboard): add random pokemon to dashboard, instead of neovim ascii art

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -19,14 +19,6 @@ return {
     })
   end,
   opts = function(_, opts)
-    opts.dashboard.preset.header = [[
-███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
-████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
-██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
-██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
-██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
-╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]]
-
     local pick = function (cmd, ops)
       return function ()
         return require('fzf-lua')[cmd](ops)
@@ -47,5 +39,39 @@ return {
       { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
       { icon = " ", key = "q", desc = "Quit", action = ":qa" },
     }
+
+    opts.dashboard.sections = {
+      { section = "keys", gap = 1, padding = 1 },
+      { section = "startup" },
+    }
+
+    opts.dashboard.width = 100
+
+    local pokemon = {
+      { name = "pikachu bulbasaur charmander squirtle", indent = 7, height = 11 },
+      { name = "pikachu chikorita cyndaquil totodile", indent = 11, height = 11 },
+      { name = "pikachu pidgey caterpie rattata", indent = 12, height = 11 },
+      { name = "jigglypuff snorlax", indent = 22, height = 20 },
+      { name = "chansey blissey", indent = 26, height = 17 },
+      { name = "eevee cubone vulpix clefairy", indent = 12, height = 11 },
+      { name = "jolteon flareon vaporeon", indent = 0, height = 17 },
+      { name = "dratini dragonair dragonite", indent = 10, height = 20 },
+      { name = "mew", indent = 40, height = 14 },
+      -- { name = "abra kadabra alakazam ", indent = 5, height = 20 },
+      -- { name = "gastly haunter gengar", indent = 7, height = 20 },
+      -- { name = "bulbasaur ivysaur venusaur", indent = 8, height = 20 },
+      -- { name = "charmander charmeleon charizard", indent = 5, height = 20 },
+      -- { name = "squirtle wartortle blastoise", indent = 10, height = 20 },
+    }
+    math.randomseed(os.time())
+    local p = pokemon[math.random(#pokemon)]
+    table.insert(opts.dashboard.sections, 1, {
+      section = "terminal",
+      cmd = "pokeget --hide-name " .. p.name,
+      indent = p.indent,
+      height = p.height,
+      ttl = 0,
+      padding = 1
+    })
   end,
 }
