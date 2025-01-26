ae83ea3021bd27ed139711fdf7090e1e46f3bdef
Hung-Yi Loo <hungyi.loo@gmail.com>
2025-01-26 16:16:00 +0800

    fix(keymaps)!: keep C-hjkl for window movement, and remap treewalker to C-M-hjkl

--- lua/plugins/treewalker.lua
+++ lua/plugins/treewalker.lua
@@ -4,9 +4,9 @@ return {
     highlight = true -- Whether to briefly highlight the node after jumping to it
   },
   keys = {
-    { mode = "n", "<C-j>", "<cmd>Treewalker Down<cr>", desc = "Treewalker Down", silent = true, nowait = true },
-    { mode = "n", "<C-k>", "<cmd>Treewalker Up<cr>", desc = "Treewalker Up", silent = true, nowait = true },
-    { mode = "n", "<C-h>", "<cmd>Treewalker Left<cr>", desc = "Treewalker Left", silent = true, nowait = true },
-    { mode = "n", "<C-l>", "<cmd>Treewalker Right<cr>", desc = "Treewalker Right", silent = true, nowait = true },
+    { mode = "n", "<C-M-j>", "<cmd>Treewalker Down<cr>", desc = "Treewalker Down", silent = true, nowait = true },
+    { mode = "n", "<C-M-k>", "<cmd>Treewalker Up<cr>", desc = "Treewalker Up", silent = true, nowait = true },
+    { mode = "n", "<C-M-h>", "<cmd>Treewalker Left<cr>", desc = "Treewalker Left", silent = true, nowait = true },
+    { mode = "n", "<C-M-l>", "<cmd>Treewalker Right<cr>", desc = "Treewalker Right", silent = true, nowait = true },
   }
 }
