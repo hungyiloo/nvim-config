38aa7263b064fbf2445049c84fc9830e93bb14d8
Hung-Yi Loo <hungyi.loo@gmail.com>
2025-05-22 20:53:59 +0700

    feat(angular): update angular treesitter parser version (my own fork)

--- lua/plugins/angular.lua
+++ lua/plugins/angular.lua
@@ -9,7 +9,7 @@ return {
       --       This coincidentally fixes inconsistent behavior in nvim-ts-autotag in htmlangular templates
       parser_config.angular.install_info.url = "https://github.com/hungyiloo/tree-sitter-angular"
       parser_config.angular.install_info.branch = "custom-fixes"
-      parser_config.angular.install_info.revision = "4e584e834b6fad8439d6996235c279545d0ca9dc"
+      parser_config.angular.install_info.revision = "124bdc2adc184bdc1659fdc5d2afe581b35ef71f"
     end
   }
 }
